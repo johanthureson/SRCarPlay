b231f8863a1a765c8ea182d8a788d537c370b887
Johan Thureson <johan.thureson@gmail.com>
2024-04-01 18:42:14 +0200

    Initial Commit

---
+++ SRCarPlay/SRCarPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRCarPlayApp.swift
+//  SRCarPlay
+//
+//  Created by Johan Thureson on 2024-04-01.
+//
+
+import SwiftUI
+
+@main
+struct SRCarPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
