3c5ed76f6a2afad0c5a3835962fd4a1789f5d8cf
Johan Thureson <johan.thureson@gmail.com>
2024-04-01 19:29:49 +0200

    controls

--- SRCarPlay/PlayView.swift
+++ SRCarPlay/PlayView.swift
@@ -12,13 +12,64 @@ struct PlayView: View {
     
     var episodes: Episodes
     @State private var player: AVPlayer?
+    @State private var isPlaying = false
 
     var body: some View {
-        Text("Hello, World!")
-            .navigationBarTitle(Text(episodes.title ?? ""), displayMode: .inline)
-            .onAppear {
-                play()
+        VStack {
+            Text("Hello, World!")
+            HStack {
+                Button(action: {
+                    player?.seek(to: .zero)
+                    player?.play()
+                    isPlaying = true
+                }) {
+                    Image(systemName: "gobackward")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+                
+                Button(action: {
+                    player?.seek(to: CMTime(seconds: player?.currentTime().seconds ?? 0 - 5, preferredTimescale: 1))
+                }) {
+                    Image(systemName: "backward.fill")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+                
+                Button(action: {
+                    if isPlaying {
+                        player?.pause()
+                    } else {
+                        player?.play()
+                    }
+                    isPlaying.toggle()
+                }) {
+                    Image(systemName: isPlaying ? "pause.circle" : "play.circle")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+                
+                Button(action: {
+                    player?.seek(to: CMTime(seconds: player?.currentTime().seconds ?? 0 + 15, preferredTimescale: 1))
+                }) {
+                    Image(systemName: "forward.fill")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+                
+                Button(action: {
+                    player?.seek(to: player?.currentItem?.asset.duration ?? .zero)
+                }) {
+                    Image(systemName: "goforward")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
             }
+        }
+        .navigationBarTitle(Text(episodes.title ?? ""), displayMode: .inline)
+        .onAppear {
+            play()
+        }
     }
     
     private func play() {
@@ -27,5 +78,6 @@ struct PlayView: View {
         
         player = AVPlayer(url: url)
         player?.play()
+        isPlaying = true
     }
 }
