a6a53caf9ce79346059df47b4ca276496eb4d42a
Johan Thureson <johan.thureson@gmail.com>
2024-04-01 18:44:43 +0200

    Tabs. Added NewsView as default tab.

---
+++ SRCarPlay/NewsView.swift
@@ -0,0 +1,18 @@
+//
+//  NewsView.swift
+//  SRCarPlay
+//
+//  Created by Johan Thureson on 2024-04-01.
+//
+
+import SwiftUI
+
+struct NewsView: View {
+    var body: some View {
+        Text("Nyheter")
+    }
+}
+
+#Preview {
+    NewsView()
+}
