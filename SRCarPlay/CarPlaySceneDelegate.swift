40f686f824f6f263aca242dc8ded115cd52cbfc1
Johan Thureson <johan.thureson@gmail.com>
2024-05-28 10:03:16 +0200

    updates according to https://developer.apple.com/carplay/documentation/CarPlay-App-Programming-Guide.pdf, but still didnt work

--- SRCarPlay/CarPlaySceneDelegate.swift
+++ SRCarPlay/CarPlaySceneDelegate.swift
@@ -5,21 +5,26 @@
 //  Created by Johan Thureson on 2024-05-28.
 //
 
-import Foundation
 import CarPlay
 
 class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
     
     var interfaceController: CPInterfaceController?
     
-    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didConnect interfaceController: CPInterfaceController) {
-        
+    // CarPlay connected
+    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
+                                  didConnect interfaceController: CPInterfaceController) {
         self.interfaceController = interfaceController
         
         self.interfaceController?.setRootTemplate(CarPlayHelloWorld().template, animated: false, completion: nil)
+
     }
     
-    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnectInterfaceController interfaceController: CPInterfaceController) {
+    // CarPlay disconnected
+    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
+                                  didDisconnectInterfaceController interfaceController: CPInterfaceController) {
         self.interfaceController = nil
     }
+
+    
 }
