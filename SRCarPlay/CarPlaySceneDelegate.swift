02595ad370254714368bad2b80c42648d3b71da5
Johan Thureson <johan.thureson@gmail.com>
2024-05-28 12:26:04 +0200

    tabs

--- SRCarPlay/CarPlaySceneDelegate.swift
+++ SRCarPlay/CarPlaySceneDelegate.swift
@@ -6,18 +6,59 @@
 import CarPlay
 
 class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
-    
     var interfaceController: CPInterfaceController?
-    
+
     func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didConnect interfaceController: CPInterfaceController) {
-        
         self.interfaceController = interfaceController
-        
-        self.interfaceController?.setRootTemplate(SRCarPlayTemlate().template, animated: false, completion: nil)
+        setupInitialTemplate()
     }
-    
-    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnectInterfaceController interfaceController: CPInterfaceController) {
+
+    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnect interfaceController: CPInterfaceController) {
         self.interfaceController = nil
     }
-    
+
+    private func setupInitialTemplate() {
+        let poddarTemplate = createPoddarTemplate()
+        let nyheterTemplate = createNyheterTemplate()
+        let kanalerTemplate = createKanalerTemplate()
+        let minSidaTemplate = createMinSidaTemplate()
+
+        let tabBarTemplate = CPTabBarTemplate(templates: [poddarTemplate, nyheterTemplate, kanalerTemplate, minSidaTemplate])
+
+        interfaceController?.setRootTemplate(tabBarTemplate, animated: true, completion: { _, _ in
+            print("Root template set")
+        })
+    }
+
+    private func createPoddarTemplate() -> CPTemplate {
+        let item = CPListItem(text: "Podcast 1", detailText: nil)
+        let section = CPListSection(items: [item])
+        let listTemplate = CPListTemplate(title: "Poddar", sections: [section])
+        listTemplate.tabImage = UIImage(systemName: "headphones")
+        return listTemplate
+    }
+
+    private func createNyheterTemplate() -> CPTemplate {
+        let item = CPListItem(text: "News 1", detailText: nil)
+        let section = CPListSection(items: [item])
+        let listTemplate = CPListTemplate(title: "Nyheter", sections: [section])
+        listTemplate.tabImage = UIImage(systemName: "doc.text")
+        return listTemplate
+    }
+
+    private func createKanalerTemplate() -> CPTemplate {
+        let item = CPListItem(text: "Channel 1", detailText: nil)
+        let section = CPListSection(items: [item])
+        let listTemplate = CPListTemplate(title: "Kanaler", sections: [section])
+        listTemplate.tabImage = UIImage(systemName: "radio.fill")
+        return listTemplate
+    }
+
+    private func createMinSidaTemplate() -> CPTemplate {
+        let item = CPListItem(text: "Profile 1", detailText: nil)
+        let section = CPListSection(items: [item])
+        let listTemplate = CPListTemplate(title: "Min sida", sections: [section])
+        listTemplate.tabImage = UIImage(systemName: "person.fill")
+        return listTemplate
+    }
 }
