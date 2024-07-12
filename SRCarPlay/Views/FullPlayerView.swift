7a1221e45610b1839611aee4be2b73787df40d8a
Johan Thureson <johan.thureson@gmail.com>
2024-07-13 00:07:40 +0200

    View vars for FullPlayerView

--- SRCarPlay/Views/FullPlayerView.swift
+++ SRCarPlay/Views/FullPlayerView.swift
@@ -16,128 +16,155 @@ struct FullPlayerView: View {
     var body: some View {
         if playerModel.state != .inActive {
             VStack {
-                HStack {
-                    Button(action: {
-                        presentationMode.wrappedValue.dismiss()
-                    }) {
-                        Image(systemName: "chevron.down.circle.fill")
-                            .resizable()
-                            .font(.system(size: 20))
-                            .padding()
-                            .frame(width: 60, height: 60)
-                    }
-                    .accessibility(label: Text("Dismiss"))
-                    Spacer()
-                }
-                .padding()
-                // Add padding at the top for safe area clearance on devices with notch
+                
+                dismissButton
                 
                 Spacer()
                 
-                if let imageUrl = playerModel.imageUrl {
-                    AsyncImage(url: imageUrl)
-                        .frame(width: 100, height: 100)
-                        .aspectRatio(contentMode: .fit)
-                }
-                Text(playerModel.title ?? "")
-                    .font(.largeTitle)
-                    .padding()
-                Text(playerModel.description ?? "")
-                    .font(.subheadline)
-                    .padding()
-                if playerModel.state == .news {
-                    VStack {
-                        HStack {
-                            Text("\(secondsToHoursMinutesSeconds(seconds: Int(playerModel.currentTime)))")
-                            Spacer()
-                            Text("-\(secondsToHoursMinutesSeconds(seconds: Int(playerModel.streamDuration - playerModel.currentTime)))")
-                        }
-                        
-                        Slider(value: playerModel.currentTimeBinding, in: 0...playerModel.streamDuration, onEditingChanged: sliderEditingChanged)
-                        
-                    }
-                    .padding()
-                    .padding(.horizontal)
-                }
+                image
                 
-                Spacer()
-                    .frame(height: 0)
+                title
                 
-                HStack(spacing: 0) {
-                    
-                    if playerModel.state == .news {
-                        
-                        Button(action: {
-                            playerModel.player?.seek(to: .zero)
-                            playerModel.player?.play()
-                            playerModel.isPlaying = true
-                        }) {
-                            Image(systemName: "backward.fill")
-                                .font(.system(size: 30))
-                                .padding(playerModel.padding)
-                        }
-                    }
-                    
-                    Button(action: {
-                        let cmTime = CMTime(seconds: (playerModel.player?.currentTime().seconds ?? 0) - playerModel.secondsBackward, preferredTimescale: 1)
-                        playerModel.player?.seek(to: cmTime)
-                    }) {
-                        ZStack {
-                            Image(systemName: "gobackward")
-                                .font(.system(size: 45))
-                                .padding(playerModel.padding)
-                            Text(String(Int(playerModel.secondsBackward)))
-                                .bold()
-                                .offset(y: 2)
-                        }
-                    }
-                    
-                    Button(action: {
-                        if playerModel.isPlaying {
-                            playerModel.player?.pause()
-                        } else {
-                            playerModel.player?.play()
-                        }
-                        playerModel.isPlaying.toggle()
-                    }) {
-                        Image(systemName: playerModel.isPlaying ? "pause.circle" : "play.circle")
-                            .font(.system(size: 60))
-                            .padding(playerModel.padding)
-                    }
-                    
-                    
-                    Button(action: {
-                        playerModel.player?.seek(to: CMTime(seconds: (playerModel.player?.currentTime().seconds ?? 0) + playerModel.secondsForward, preferredTimescale: 1))
-                    }) {
-                        ZStack {
-                            Image(systemName: "goforward")
-                                .font(.system(size: 45))
-                                .padding(playerModel.padding)
-                            Text(String(Int(playerModel.secondsForward)))
-                                .bold()
-                                .offset(y: 2)
-                        }
-                    }
-                    if playerModel.state == .news {
-                        
-                        Button(action: {
-                            playerModel.player?.seek(to: playerModel.player?.currentItem?.duration ?? .zero)
-                        }) {
-                            Image(systemName: "forward.fill")
-                                .font(.system(size: 30))
-                                .padding(playerModel.padding)
-                        }
-                    }
-                }
+                description
+                
+                timeLine
+                
+                playerControls
                 
                 Spacer()
                 
             }
             .navigationBarTitle(Text(playerModel.title ?? ""), displayMode: .inline)
         }
-        
     }
     
+    private var dismissButton: some View {
+        HStack {
+            Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(systemName: "chevron.down.circle.fill")
+                    .resizable()
+                    .font(.system(size: 20))
+                    .padding()
+                    .frame(width: 60, height: 60)
+            }
+            .accessibility(label: Text("Dismiss"))
+            Spacer()
+        }
+        .padding()
+    }
+    
+    @ViewBuilder
+    private var image: some View {
+        if let imageUrl = playerModel.imageUrl {
+            AsyncImage(url: imageUrl)
+                .frame(width: 100, height: 100)
+                .aspectRatio(contentMode: .fit)
+        }
+    }
+
+    private var title: some View {
+        Text(playerModel.title ?? "")
+            .font(.largeTitle)
+            .padding()
+    }
+
+    private var description: some View {
+        Text(playerModel.description ?? "")
+            .font(.subheadline)
+            .padding()
+
+    }
+    
+    @ViewBuilder
+    private var timeLine: some View {
+        if playerModel.state == .news {
+            VStack {
+                HStack {
+                    Text("\(secondsToHoursMinutesSeconds(seconds: Int(playerModel.currentTime)))")
+                    Spacer()
+                    Text("-\(secondsToHoursMinutesSeconds(seconds: Int(playerModel.streamDuration - playerModel.currentTime)))")
+                }
+                
+                Slider(value: playerModel.currentTimeBinding, in: 0...playerModel.streamDuration, onEditingChanged: sliderEditingChanged)
+                
+            }
+            .padding()
+            .padding(.horizontal)
+        }
+    }
+    
+    private var playerControls: some View {
+        
+        HStack(spacing: 0) {
+            
+            if playerModel.state == .news {
+                
+                Button(action: {
+                    playerModel.player?.seek(to: .zero)
+                    playerModel.player?.play()
+                    playerModel.isPlaying = true
+                }) {
+                    Image(systemName: "backward.fill")
+                        .font(.system(size: 30))
+                        .padding(playerModel.padding)
+                }
+            }
+            
+            Button(action: {
+                let cmTime = CMTime(seconds: (playerModel.player?.currentTime().seconds ?? 0) - playerModel.secondsBackward, preferredTimescale: 1)
+                playerModel.player?.seek(to: cmTime)
+            }) {
+                ZStack {
+                    Image(systemName: "gobackward")
+                        .font(.system(size: 45))
+                        .padding(playerModel.padding)
+                    Text(String(Int(playerModel.secondsBackward)))
+                        .bold()
+                        .offset(y: 2)
+                }
+            }
+            
+            Button(action: {
+                if playerModel.isPlaying {
+                    playerModel.player?.pause()
+                } else {
+                    playerModel.player?.play()
+                }
+                playerModel.isPlaying.toggle()
+            }) {
+                Image(systemName: playerModel.isPlaying ? "pause.circle" : "play.circle")
+                    .font(.system(size: 60))
+                    .padding(playerModel.padding)
+            }
+            
+            
+            Button(action: {
+                playerModel.player?.seek(to: CMTime(seconds: (playerModel.player?.currentTime().seconds ?? 0) + playerModel.secondsForward, preferredTimescale: 1))
+            }) {
+                ZStack {
+                    Image(systemName: "goforward")
+                        .font(.system(size: 45))
+                        .padding(playerModel.padding)
+                    Text(String(Int(playerModel.secondsForward)))
+                        .bold()
+                        .offset(y: 2)
+                }
+            }
+            if playerModel.state == .news {
+                
+                Button(action: {
+                    playerModel.player?.seek(to: playerModel.player?.currentItem?.duration ?? .zero)
+                }) {
+                    Image(systemName: "forward.fill")
+                        .font(.system(size: 30))
+                        .padding(playerModel.padding)
+                }
+            }
+        }
+    }
+
     
     private func sliderEditingChanged(editingStarted: Bool) {
         if playerModel.timeControlStatus == nil {
