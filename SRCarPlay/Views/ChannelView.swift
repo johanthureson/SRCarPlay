207af7b89c439c549c0a7f9cd3405aafd10ce9e1
Johan Thureson <johan.thureson@gmail.com>
2024-07-12 14:28:12 +0200

    View vars

--- SRCarPlay/Views/ChannelView.swift
+++ SRCarPlay/Views/ChannelView.swift
@@ -18,30 +18,8 @@ struct ChannelView: View {
         
         List {
             Section {
-                ForEach(channels, id: \.self) { channel in
-                    Button(action: {
-                        self.playerModel.state = .channel
-                        self.playerModel.initWith(channel: channel)
-                        self.playerModel.play()
-                    }) {
-                        HStack {
-                            if let imageUrl = channel.image, let url = URL(string: imageUrl) {
-                                AsyncImage(url: url)
-                                    .frame(width: 50, height: 50)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                            }
-                            Spacer().frame(width: 16)
-                            VStack(alignment: .leading) {
-                                Text(channel.name ?? "")
-                                    .font(.headline)
-                                Text(channel.tagline ?? "")
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
-                                    .lineLimit(2)
-                            }
-                        }
-                    }
-                }
+                
+                channelList
                 
             } footer: {
                 
@@ -57,12 +35,41 @@ struct ChannelView: View {
         
     }
     
+    private var channelList: some View {
+        ForEach(channels, id: \.self) { channel in
+            Button(action: {
+                self.playerModel.state = .channel
+                self.playerModel.initWith(channel: channel)
+                self.playerModel.play()
+            }) {
+                HStack {
+                    if let imageUrl = channel.image, let url = URL(string: imageUrl) {
+                        AsyncImage(url: url)
+                            .frame(width: 50, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    Spacer().frame(width: 16)
+                    VStack(alignment: .leading) {
+                        Text(channel.name ?? "")
+                            .font(.headline)
+                        Text(channel.tagline ?? "")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .lineLimit(2)
+                    }
+                }
+            }
+        }
+    }
+    
     private func loadChannels() {
         guard let url = URL(string: Constants.getUrlStringFor(path: "channels")) else {
             print("Invalid URL")
             return
         }
+        
         let request = URLRequest(url: url)
+        
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let data = data {
                 if let decodedResponse = try? JSONDecoder().decode(ChannelsResponse.self, from: data) {
