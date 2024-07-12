f6831992c23653b727113d1f136a5e8659107461
Johan Thureson <johan.thureson@gmail.com>
2024-07-13 00:29:42 +0200

    private vars for views in MiniPlayerView

--- SRCarPlay/ViewComponents/MiniPlayerView.swift
+++ SRCarPlay/ViewComponents/MiniPlayerView.swift
@@ -21,41 +21,25 @@ struct MiniPlayerView: View {
             HStack {
                 
                 HStack {
-                    Spacer()
-                        .frame(width: 16)
-                    Image(systemName: "chevron.up.circle.fill")
-                        .resizable()
-                        .font(.system(size: 20))
-                        .frame(width: 30, height: 30)
-                        .padding(.vertical)
                     
-                    Text(playerModel.title ?? " ")
-                        .padding()
-                    Spacer()
+                    upArrow
+
+                    title
+                    
                 }
                 // Make the entire HStack tappable
                 .contentShape(Rectangle())
+                
                 .onTapGesture {
                     showFullPlayer = true // Trigger to show the modal
                 }
-                .fullScreenCover(isPresented: $showFullPlayer) { // 2. Use the .sheet modifier
-                    FullPlayerView() // 3. Present FullPlayerView as a modal
+                
+                .fullScreenCover(isPresented: $showFullPlayer) {
+                    FullPlayerView()
                         .accentColor(.black)
                 }
                 
-                // Play/Pause button
-                Button(action: {
-                    if playerModel.isPlaying {
-                        playerModel.player?.pause()
-                    } else {
-                        playerModel.player?.play()
-                    }
-                    playerModel.isPlaying.toggle()
-                }) {
-                    Image(systemName: playerModel.isPlaying ? "pause.circle" : "play.circle")
-                        .font(.system(size: 30))
-                        .padding(.horizontal)
-                }
+                playPauseButton
                 
             }
             
@@ -66,4 +50,38 @@ struct MiniPlayerView: View {
         .frame(height: 64)
         
     }
+    
+    @ViewBuilder
+    private var upArrow: some View {
+        Spacer()
+            .frame(width: 16)
+        Image(systemName: "chevron.up.circle.fill")
+            .resizable()
+            .font(.system(size: 20))
+            .frame(width: 30, height: 30)
+            .padding(.vertical)
+    }
+    
+    @ViewBuilder
+    private var title: some View {
+        Text(playerModel.title ?? " ")
+            .padding()
+        Spacer()
+    }
+    
+    private var playPauseButton: some View {
+        Button(action: {
+            if playerModel.isPlaying {
+                playerModel.player?.pause()
+            } else {
+                playerModel.player?.play()
+            }
+            playerModel.isPlaying.toggle()
+        }) {
+            Image(systemName: playerModel.isPlaying ? "pause.circle" : "play.circle")
+                .font(.system(size: 30))
+                .padding(.horizontal)
+        }
+    }
+    
 }
